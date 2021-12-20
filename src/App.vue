06ac3d0a8bf103e45654d3caddb3115f68252f37
tiborhercz <tibor@hercz.nl>
2021-12-20 20:35:14 +0100

    add workflow

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
   <v-app>
     <v-navigation-drawer app>
-      <Drawer/>
+      <Drawer />
     </v-navigation-drawer>
-
-    <v-app-bar app>
-      <!-- -->
-    </v-app-bar>
-
-    <!-- Sizes your content based upon application components -->
     <v-main>
-
-      <!-- Provides the application the proper gutter -->
       <v-container fluid>
-
-        <!-- If using vue-router -->
-        <router-view></router-view>
+        <router-view />
       </v-container>
     </v-main>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
